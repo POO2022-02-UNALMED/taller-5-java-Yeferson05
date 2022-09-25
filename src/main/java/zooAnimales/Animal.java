3b174cff8d7c16cda03a5ac75baeec84abd948df
Yeferson05 <72055204+Yeferson05@users.noreply.github.com>
2022-09-25 16:13:27 -0500

    Add files via upload

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -81,11 +81,11 @@ public class Animal {
     public String toString() {
     	if(zona != null) {
     		return "Mi nombre es" + nombre + ", tengo una edad de" + edad + 
-    				", habito en"+ habitat + "y mi genero es" + genero + 
-    				"la zona en la que me hubico es" + zona;
+    				", habito en" + habitat + "y mi genero es" + genero + 
+    				", la zona en la que me ubico es" + zona + ", en el" + zona.getZoo();
     	}
     	else{
-    		return "Mi nombre es" + nombre + "tengo una edad de" + edad + "habito en"+ habitat;
+    		return "Mi nombre es" + nombre + ", tengo una edad de" + edad + ", habito en" + habitat + "y mi genero es" + genero ;
     	}
     }
     
